0e573547eb6baa9ef88045137f3a6de9954cfc2e
drageryort <drageryort@gmail.com>
2019-03-31 12:06:41 +0300

    add shop template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
-const App = () =>(
-	<div>
-		<h1>Hello world</h1>
-  		<p>Hello world</p>
-  	</div>
-  	)
+const App = () =>{
+	return(
+		<div>
+			<header className="header">
+				<div className="conteiner">
+					<div className="row">
+						<div className="col-md-3">
+							<div className="logo">
+								<img src="img/logo.png" alt=""/>
+							</div>
+						</div>
+						<div className="col-md-6">
+							<div className="menu">
+								<ul>
+									<li><a href="">Home</a></li>
+									<li><a href="">Product</a></li>
+									<li><a href="">Payment</a></li>
+									<li><a href="">Shipping</a></li>
+								</ul>
+							</div>
+						</div>
+						<div className="col-md-3">
+							<div className="cart text-center">
+								<div className="products-count">0</div>
+								<div className="products-price">$500</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+	)
+}
 export default App

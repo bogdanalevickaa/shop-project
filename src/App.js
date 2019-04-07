4c4111bfaa064da47b021cfd41d4f83b8ad35340
drageryort <drageryort@gmail.com>
2019-04-07 11:41:52 +0300

    +Footer.js, Header.js,Main.js

--- src/App.js
+++ src/App.js
@@ -7,66 +7,9 @@ import './header.css'
 import './cart.css'
 import './menu.css'
 
-const Header = () =>{
-	return(
-		<div>
-			<header className="header">
-				<div className="conteiner">
-					<div className="row">
-						<div className="col-lg-3">
-							<div className="logo">
-								<img src="img/logo.png" alt=""/>
-							</div>
-						</div>
-						<div className="col-lg-6">
-							<div className="menu">
-								<ul>
-									<li><a href="">Home</a></li>
-									<li><a href="">Product</a></li>
-									<li><a href="">Payment</a></li>
-									<li><a href="">Shipping</a></li>
-								</ul>
-							</div>
-						</div>
-						<div className="col-lg-3">
-							<div className="cart text-center">
-								<div className="products-count">0</div>
-								<div className="products-price">$500</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</header>
-		</div>
-	)
-}
-const Main = () =>{
-	return(
-		<div>
-			<main className="main">
-				<div className="conteiner">
-					<div className="row">
-						<div className="col-lg-3">
-							Filter
-						</div>
-						<div className="col-lg-9">
-							Products List
-						</div>
-					</div>
-				</div>
-			</main>
-		</div>
-	)
-}
-const Footer = () =>{
-	return(
-		<div>
-			<footer className="Footer">
-				Footer
-			</footer>
-		</div>
-	)
-}
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 const App =() => {
 	return(

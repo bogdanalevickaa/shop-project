fd62a3f1e5612d549b36d29238eafbc030bc2f01
drageryort <drageryort@gmail.com>
2019-04-13 11:42:36 +0300

    add array products

---
+++ src/App/Main/Products/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        name:"iPhone 7",
+        description:"This is iPhone 7",
+        type:"phone",
+        capacity:"256",
+        price:"800",
+    },
+    {
+        name:"iPhone X",
+        description:"This is iPhone X",
+        type:"phone",
+        capacity:"256",
+        price:"1200",
+    },
+    {
+        name:"iPhone 8",
+        description:"This is iPhone 8",
+        type:"phone",
+        capacity:"256",
+        price:"800",
+    },
+    {
+        name:"iPhone 8 plus",
+        description:"This is iPhone 8 plus",
+        type:"phone",
+        capacity:"256",
+        price:"900",
+    },
+]
+export default products

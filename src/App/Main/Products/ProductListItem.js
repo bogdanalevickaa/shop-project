1c2b9b30812bf19f44fc2fc325b438cec7bb38fe
drageryort <drageryort@gmail.com>
2019-04-13 11:51:19 +0300

    destructuring

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -2,15 +2,20 @@ import React from 'react'
 import "./ProductListItem.css"
 
 
-const ProductListItem = (props) =>{
-    console.log(props)
+const ProductListItem = ({
+    name,
+    description,
+    type,
+    capacity,
+    price
+}) =>{
     return(
         <div className="product-list-item">
-            <h2 className="product-title">{props.name}</h2>
-            <div className="product-description">{props.description}</div>
-            <div className="product-features">Tupe: {props.type}</div>
-            <div className="product-features">Capacity: {props.capacity} Gb</div>
-            <div className="product-price">$ {props.price}</div>
+            <h2 className="product-title">{name}</h2>
+            <div className="product-description">{description}</div>
+            <div className="product-features">Tupe: {type}</div>
+            <div className="product-features">Capacity: {capacity} Gb</div>
+            <div className="product-price">$ {price}</div>
             <button className="btn btn-add-to-cart">Add to cart</button>
         </div>
     )

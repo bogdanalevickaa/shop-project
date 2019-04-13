e2e5546959c13d9ae199380ca60c664dd0bbf148
drageryort <drageryort@gmail.com>
2019-04-13 12:20:32 +0300

    metod map

--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import ProductListItem from './ProductListItem';
+import products from './products';
 
 const ProductsList = () => {
     return(
         <div>
             <h1 className="title-page">Products List</h1>
             <div className="row">
-                <div className="col-lg-6">
+            {
+                products.map((product)=>{
+                    return(
+                        <div className="col-lg-6">
+                            <ProductListItem
+                                name={product.name}
+                                description={product.description}
+                                type={product.type}
+                                capacity={product.capacity}
+                                price={product.price}
+                            />
+                        </div>
+                    )
+                })
+            }
+                {/* <div className="col-lg-6">
                     <ProductListItem
                         name="iPhone 7"
                         description="This is iPhone 7"
@@ -14,34 +30,7 @@ const ProductsList = () => {
                         capacity="256"
                         price="800"
                     />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem
-                        name="iPhone X"
-                        description="This is iPhone X"
-                        type="phone"
-                        capacity="256"
-                        price="1200"
-                    />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem
-                        name="iPhone 8"
-                        description="This is iPhone 8"
-                        type="phone"
-                        capacity="256"
-                        price="800"
-                    />
-                </div>
-                <div className="col-lg-6">
-                    <ProductListItem
-                        name="iPhone 8 plus"
-                        description="This is iPhone 8 plus"
-                        type="phone"
-                        capacity="256"
-                        price="900"
-                    />
-                </div>
+                </div> */}
             </div>
         </div>
     )

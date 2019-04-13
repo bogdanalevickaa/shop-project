10df00be50bcda25e4c156d122951dc439ccf16a
drageryort <drageryort@gmail.com>
2019-04-13 10:43:42 +0300

    add props

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -7,10 +7,10 @@ const ProductListItem = (props) =>{
     return(
         <div className="product-list-item">
             <h2 className="product-title">{props.name}</h2>
-            <div className="product-description">This is iPhone X</div>
-            <div className="product-features">Tupe: phone</div>
-            <div className="product-features">Capacity: 64Gb</div>
-            <div className="product-price">$ 600</div>
+            <div className="product-description">{props.description}</div>
+            <div className="product-features">Tupe: {props.type}</div>
+            <div className="product-features">Capacity: {props.capacity} Gb</div>
+            <div className="product-price">$ {props.price}</div>
             <button className="btn btn-add-to-cart">Add to cart</button>
         </div>
     )
--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -9,21 +9,37 @@ const ProductsList = () => {
                 <div className="col-lg-6">
                     <ProductListItem
                         name="iPhone 7"
+                        description="This is iPhone 7"
+                        type="phone"
+                        capacity="256"
+                        price="800"
                     />
                 </div>
                 <div className="col-lg-6">
                     <ProductListItem
                         name="iPhone X"
+                        description="This is iPhone X"
+                        type="phone"
+                        capacity="256"
+                        price="1200"
                     />
                 </div>
                 <div className="col-lg-6">
                     <ProductListItem
                         name="iPhone 8"
+                        description="This is iPhone 8"
+                        type="phone"
+                        capacity="256"
+                        price="800"
                     />
                 </div>
                 <div className="col-lg-6">
                     <ProductListItem
                         name="iPhone 8 plus"
+                        description="This is iPhone 8 plus"
+                        type="phone"
+                        capacity="256"
+                        price="900"
                     />
                 </div>
             </div>

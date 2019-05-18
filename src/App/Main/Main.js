4876a5076b071eaf2f1e53e868ffeff81b957526
dana <bogdanalevickaa@gmail.com>
2019-05-18 11:38:07 +0300

    Link Shipping and Payment

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 import CartPage from './CartPage/CartPage'
+import Payment from './Payment/Payment'
+import Shipping from './Shipping/Shipping'
 import ProductsList from './Products/ProductsList'
 
 const Main = ({
@@ -14,15 +16,14 @@ const Main = ({
 						Filter
 					</div>
 					<div className="col-lg-9">
-						{/* <ProductsList
-							addProductToCart={addProductToCart}
-						/> */}
 						<Route exact path="/" render={()=>{
 							return <ProductsList
 								addProductToCart={addProductToCart}
 							/>
 						}}/>
 						<Route path="/cart" component={CartPage}/>
+						<Route path="/payment" component={Payment}/>
+						<Route path="/shipping" component={Shipping}/>
 					</div>
 				</div>
 			</div>
---
+++ src/App/Main/Shipping/Shipping.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Shipping = () =>{
+    return(
+        <h1 className="title-page">Shipping</h1>
+    )
+}
+export default Shipping;
